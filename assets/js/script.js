cedb12458c5c5acc9126f6f09f45065a4ec24cb5
ABDULLAH SIDDIQUI <abdullah.siddiqui13122002@gmail.com>
2024-06-03 14:11:50 +0500

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,138 @@
+// slider
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.mySwiper', {
+    direction: "vertical",
+    slidesPerView: 1,
+    mousewheel: true,
+    speed: 1000,
+    pagination: {
+      el: '.pagination ul',
+      clickable: true,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    on: {
+      init: function () {
+        animateBannerText(this.slides[this.activeIndex]);
+      },
+      slideChangeTransitionStart: function () {
+        gsap.to('.banner-text', { opacity: 0, x: -200, duration: 0.5 });
+      },
+      slideChangeTransitionEnd: function () {
+        animateBannerText(this.slides[this.activeIndex]);
+      },
+      slideChange: function () {
+        const activeIndex = this.realIndex + 1;
+        const paginationNumbers = document.querySelectorAll('.pagination ul li');
+        paginationNumbers.forEach((number, index) => {
+          number.textContent = pad(index + 1, 2); // Pad the number with 2 digits
+          number.classList.remove('active-page');
+          if (index === activeIndex - 1) {
+            number.classList.add('active-page');
+            updateDotPosition(number); // Update dot position based on active page
+          }
+        });
+
+        // Disable mousewheel and keyboard navigation on the last slide
+        if (this.realIndex === this.slides.length - 1) {
+          this.mousewheel.disable();
+          this.keyboard.disable();
+        } else {
+          this.mousewheel.enable();
+          this.keyboard.enable();
+        }
+      },
+    },
+  });
+
+  // Populate pagination numbers
+  const paginationList = document.querySelector('.pagination ul');
+  for (let i = 0; i < swiper.slides.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = pad(i + 1, 2); // Pad the number with 2 digits
+    paginationList.appendChild(listItem);
+  }
+
+  // Function to pad numbers with leading zeros
+  function pad(number, length) {
+    return String(number).padStart(length, '0');
+  }
+  // Function to update dot position based on active page
+  function updateDotPosition(activePage) {
+    const dot = document.querySelector('.dot');
+    const activePageIndex = [...activePage.parentNode.children].indexOf(activePage); // Get index of active page
+    const dotTop = 7 + (activePageIndex * 6); // Calculate top position
+    dot.style.top = `${dotTop}%`; // Set top position of the dot
+  }
+
+  // Function to animate banner text
+  function animateBannerText(activeSlide) {
+    gsap.fromTo(activeSlide.querySelector('.banner-text'), 
+    { opacity: 0, x: -200 },
+    { opacity: 1, x: 0, duration: 0.6, delay: 0.3 }
+    );
+  }
+});
+
+var tl = gsap.timeline();
+
+tl.from("nav .navbar-brand, nav .nav-item, nav .rightdiv",{
+  y: -40,
+  duration: 0.7,
+  delay: 0.5,
+  opacity: 0,
+  stagger: 0.15
+})
+
+tl.from(".banner .banner-text",{
+  opacity: 0,
+  x: -200,
+  stagger: 0.15,
+  duration: 0.6
+})
+
+tl.from(".pagination",{
+  opacity: 0,
+  x: 100,
+  stagger: 0.15,
+  duration: 0.6
+})
+
+tl.from(".social li",{
+  opacity: 0,
+  y: 30,
+  stagger: 0.15,
+  duration: 0.6
+})
+
+
+// slider
+
+
+// card slider
+
+const carousel3Dswiper = new Swiper(".carousel-3D-swiper", {
+  loop: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 1200,
+    modifier: 1,
+    slideShadows: true
+  },
+   navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
+
+// card slider
+
